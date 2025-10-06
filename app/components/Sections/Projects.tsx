c1cffa6874a11d0dbbb946830413d01b79d15e45
Leon Bryan <leonard.bryan.email@gmail.com>
2025-10-07 00:10:24 +0800

    Added Carousel for Projects section

--- app/components/Sections/Projects.tsx
+++ app/components/Sections/Projects.tsx
@@ -1,11 +1,12 @@
 "use client";
 
 import Image from "next/image";
+import ProjectCard from "../ProjectCard";
 
 const Projects = () => {
   return (
     <div className="flex flex-row md:flex-col md:justify-center h-full w-full justify-between items-center gap-5 px-15 pb-10">
-        asd
+        <ProjectCard />
     </div>
   );
 };
---
+++ app/components/data/Projects.ts
@@ -0,0 +1,23 @@
+export interface IProject {
+  id: number;
+  name: string;
+}
+
+export const ProjectData: IProject[] = [
+  {
+    id: 1,
+    name: "Guitar Habit Music Hub",
+  },
+  {
+    id: 2,
+    name: "Tipidicuts",
+  },
+  {
+    id: 3,
+    name: "Erl, The Bird Landing Page",
+  },
+  {
+    id: 4,
+    name: "LeSource",
+  },
+];
